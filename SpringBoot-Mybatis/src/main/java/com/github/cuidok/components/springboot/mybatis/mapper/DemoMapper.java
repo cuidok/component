cfaef2202f778e68739e2872ce4a625911017013
cuidok <zapwe@qq.com>
2023-04-17 09:31:51 +0800

    Add SpringBoot template built with MyBatis

---
+++ SpringBoot-Mybatis/src/main/java/com/github/cuidok/components/springboot/mybatis/mapper/DemoMapper.java
@@ -0,0 +1,10 @@
+package com.github.cuidok.components.springboot.mybatis.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface DemoMapper {
+
+}
