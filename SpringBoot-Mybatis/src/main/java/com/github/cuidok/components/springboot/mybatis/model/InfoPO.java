8de4eeef32622c4cd822c9aac64505e08417fc16
cuidok <zapwe@qq.com>
2023-04-18 10:09:54 +0800

    Add info PO model

---
+++ SpringBoot-Mybatis/src/main/java/com/github/cuidok/components/springboot/mybatis/model/InfoPO.java
@@ -0,0 +1,36 @@
+package com.github.cuidok.components.springboot.mybatis.model;
+
+import java.time.LocalDateTime;
+
+public class InfoPO {
+
+    private Long id;
+
+    private String info;
+
+    private LocalDateTime createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+}
