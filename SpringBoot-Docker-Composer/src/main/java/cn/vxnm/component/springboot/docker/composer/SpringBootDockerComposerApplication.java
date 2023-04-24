717efe2fb93b992f05a44f554fc8341edc81c9ac
cuidok <zapwe@qq.com>
2023-04-24 11:06:49 +0800

    Add SpringBoot plan which is code template build with docker composer

---
+++ SpringBoot-Docker-Composer/src/main/java/cn/vxnm/component/springboot/docker/composer/SpringBootDockerComposerApplication.java
@@ -0,0 +1,13 @@
+package cn.vxnm.component.springboot.docker.composer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerComposerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerComposerApplication.class, args);
+    }
+
+}
