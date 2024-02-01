d3669215610c0a01fa83ea6e0cdfb2ce6e2bdd30
cuidok <zapwe@qq.com>
2024-02-01 09:32:19 +0800

    Use parameters to replace hard-coded in docker-deploy/redis

--- docker-deploy/redis/7.0.x-cluster/create-clus.sh
+++ docker-deploy/redis/7.0.x-cluster/create-clus.sh
@@ -1,4 +1,12 @@
-docker exec -it redis-c-7000 \
- redis-cli --cluster create 172.17.0.1:7000 172.17.0.1:7001 172.17.0.1:7002 \
-  172.17.0.1:7003 172.17.0.1:7004 172.17.0.1:7005 \
-   --cluster-replicas 1
+# REDIS_CLUSTER_ENTRYPOINT is the name of the container where you want to execute the redis-cli command
+# REDIS_1 to REDIS_6 are the addresses of your Redis cluster nodes
+REDIS_CLUSTER_ENTRYPOINT=redis-c-7000
+REDIS_1=172.17.0.1:7000
+REDIS_2=172.17.0.1:7001
+REDIS_3=172.17.0.1:7002
+REDIS_4=172.17.0.1:7003
+REDIS_5=172.17.0.1:7004
+REDIS_6=172.17.0.1:7005
+
+docker exec -it $REDIS_CLUSTER_ENTRYPOINT \
+ redis-cli --cluster create $REDIS_1 $REDIS_2 $REDIS_3 $REDIS_4 $REDIS_5 $REDIS_6 --cluster-replicas 1
