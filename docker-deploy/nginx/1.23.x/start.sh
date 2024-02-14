5e3fcb5760932e58e3f06cc7308404ebcf5fd390
cuidok <zapwe@qq.com>
2024-02-14 13:47:22 +0800

    Add the parameter comment to the nginx\start.sh script

--- docker-deploy/nginx/1.23.x/start.sh
+++ docker-deploy/nginx/1.23.x/start.sh
@@ -1,9 +1,22 @@
-# Container name
-NAME=nginx
-# Server port
+# This is the name of the Docker container that will be created
+CONTAINER_NAME=nginx
+
+# This is the port that the Nginx server will listen on
 SERVER_PORT=80
 
-docker run -d --name $NAME \
-  -p $SERVER_PORT:80 \
-  -v conf.d:/etc/nginx/conf.d \
+# This is the path where Nginx configuration files will be stored
+DATA_PATH=conf.d
+
+# This determines when the Docker container should be restarted
+# no - No restart
+# on-failure[:max-retries] - Restart when exit code is not 0, try at most max-retries times
+# unless-stopped - Always restart unless container is explicitly stopped or Docker itself is stopped or restarted.
+# always - Always restart
+RESTART=always
+
+# Command to run the Docker container
+docker run -d --name "$CONTAINER_NAME" \
+  -p "$SERVER_PORT:80" \
+  -v "$DATA_PATH:/etc/nginx/conf.d" \
+  --restart="$RESTART" \
   nginx:1.23.3
