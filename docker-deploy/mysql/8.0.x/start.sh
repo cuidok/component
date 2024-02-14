f9eaa49106732ca8413c1ff05155ff863e70bac0
cuidok <zapwe@qq.com>
2024-02-14 13:45:02 +0800

    Add the parameter comment to the mysql\start.sh script

--- docker-deploy/mysql/8.0.x/start.sh
+++ docker-deploy/mysql/8.0.x/start.sh
@@ -1,23 +1,30 @@
-# Container name
-NAME=mysql_8
-# Server port
+# This is the name of the Docker container that will be created
+CONTAINER_NAME=mysql_8
+
+# This is the port that the MySQL server will listen on
 SERVER_PORT=3306
+
+# This is the port that the MySQL X Server will listen on
 MYSQL_X_SERVER_PORT=33060
-# Password of root
+
+# This is the password for the root user of the MySQL server
 PASSWORD=Mysql@123456
-# Data path
+
+# This is the path where MySQL data will be stored
 DATA_PATH=/var/lib/mysql
-# no - No restart
-# on-failure[:max-retries] - Restart when exit code is not 0, try at most max-retries times
-# unless-stopped - Always restart unless container is explicitly stopped or Docker itself is stopped or restarted.
-# always - Always restart
+
+# Restart policy, defines how Docker should handle the container when it exits
+# "no" - This is the default setting. The container will not be automatically restarted when it exits.
+# "on-failure[:max-retries]" - Docker will only try to restart the container if it exits with a non-zero status. If an optional max-retries is provided, Docker will try to restart the container until it reaches the maximum number of retries.
+# "always" - Docker will always try to restart the container when it exits, regardless of the exit status.
+# "unless-stopped" - Docker will always try to restart the container when it exits, regardless of the exit status, unless the user explicitly stops the container, or Docker itself is stopped or restarted.
 RESTART=always
 
 # Command
-docker run -d --name $NAME \
-  -p $SERVER_PORT:3306 \
-  -p $MYSQL_X_SERVER_PORT:33060 \
-  -e MYSQL_ROOT_PASSWORD=$PASSWORD \
-  -v $DATA_PATH:/var/lib/mysql \
-  --restart=$RESTART \
+docker run -d --name "$CONTAINER_NAME" \
+  -p "$SERVER_PORT:3306" \
+  -p "$MYSQL_X_SERVER_PORT:33060" \
+  -e "MYSQL_ROOT_PASSWORD=$PASSWORD" \
+  -v "$DATA_PATH:/var/lib/mysql" \
+  --restart="$RESTART" \
   mysql:8.0.33
