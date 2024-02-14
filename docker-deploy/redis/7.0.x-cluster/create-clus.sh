c1fdbd72ecdefd392d84e06bf595732d1327e567
cuidok <zapwe@qq.com>
2024-02-14 13:54:10 +0800

    Adjust the parameter comment in redis/create-clus.sh

--- docker-deploy/redis/7.0.x-cluster/create-clus.sh
+++ docker-deploy/redis/7.0.x-cluster/create-clus.sh
@@ -1,6 +1,7 @@
 # REDIS_CLUSTER_ENTRYPOINT is the name of the container where you want to execute the redis-cli command
-# REDIS_1 to REDIS_6 are the addresses of your Redis cluster nodes
 REDIS_CLUSTER_ENTRYPOINT=redis-c-7000
+
+# REDIS_1 to REDIS_6 are the addresses of your Redis cluster nodes
 REDIS_1=172.17.0.1:7000
 REDIS_2=172.17.0.1:7001
 REDIS_3=172.17.0.1:7002
